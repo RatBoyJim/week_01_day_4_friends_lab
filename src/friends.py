1c71e697e1537d7c1219d735556dd6fdfc1158f9
Ruth Ironside <ruthironside@live.com>
2020-10-15 11:16:25 +0100

    test 3.

--- src/friends.py
+++ src/friends.py
@@ -3,3 +3,9 @@ def get_name(name):
 
 def get_favourite_tv_show(show):
     return show["tv_show"]
+
+def test_person_likes_food__True(food, person):
+    if person["snacks"] == food:
+        return True
+    else:
+        return False
